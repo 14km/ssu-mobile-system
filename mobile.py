5a259950537383382fe98a72b9bf86fb020c56bc
14km <k.connor614@gmail.com>
2021-12-21 23:13:49 +0900

    init

---
+++ mobile.py
@@ -0,0 +1,15 @@
+import boto3
+
+# AWS를 이용한 전화 걸기
+# client = boto3.client('connect')
+# client.start_outbound_voice_contact(
+#     DestinationPhoneNumber='+821082085996',
+#     ContactFlowId='',
+#     InstanceId='',
+#     SourcePhoneNumber='+18333912774',
+#     AnswerMachineDetectionConfig={
+#         'EnableAnswerMachineDetection': False,
+#         'AwaitAnswerMachinePrompt': False
+#     },
+# )
+
