749160fa410b61a078f9de0fa2a88d5a3e1dd35d
14km <k.connor614@gmail.com>
2021-12-22 22:21:05 +0900

    아침인 경우 연락 하도록 수정

--- mobile.py
+++ mobile.py
@@ -1,10 +1,9 @@
 import boto3
-from botocore.config import Config
 import time
 import smtplib
+import smbus
 import os
 from dotenv import load_dotenv
-
 from email.mime.text import MIMEText
 
 # 크롤링을 위한 selenium
@@ -20,12 +19,9 @@ SOURCE_PHONE_NUMBER = os.getenv('SOURCE_PHONE_NUMBER')
 DESTINATION_PHONE_NUMBER = os.getenv('DESTINATION_PHONE_NUMBER')
 SMTP_PW = os.getenv('SMTP_PW')
 SMTP_EMAIL = os.getenv('SMTP_MAIL')
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID'),
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-botoConfig = Config(
-    region_name='ap-northeast-2'
-)
 
 def isMorning():
     morning = time.strftime('%H:%M:%S', time.localtime(time.time()))
@@ -36,9 +32,9 @@ def isMorning():
 def callToNumber():
     # AWS를 이용한 전화 걸기
     client = boto3.client('connect',
-                          config=botoConfig,
                           aws_access_key_id=AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          region_name='ap-northeast-2'
                           )
     client.start_outbound_voice_contact(
         DestinationPhoneNumber=DESTINATION_PHONE_NUMBER,
@@ -83,3 +79,21 @@ def runEmailSend(msg):
     smtp.sendmail(SMTP_EMAIL, SMTP_EMAIL, msg.as_string())
     smtp.quit()
     print("[2] 이메일 발송 완료")
+
+
+address = 0x48
+AIN0 = 0x40
+
+bus = smbus.SMBus(1)
+
+while True:
+    morning = time.strftime('%H:%M:%S', time.localtime(time.time()))
+
+    bus.write_byte(address, AIN0)
+    value = bus.read_byte(address)
+    time.sleep(0.1)
+
+    if value <= 250 and morning == '08:00:00':
+        print("아침입니다!!!! 일어나세요!!!! ")
+        runNewsCrawlingByNaver()
+        callToNumber()
